0eb2b011baf7b3ca03365ce6c41a3fbdfc609aad
aries0d0f <aries0d0f@gmail.com>
2023-01-29 03:31:16 +0800

    feat(comp): Add basic component sets.

---
+++ src/modules/components/Element/MainFrame.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <RouterView name="CGXView"></RouterView>
+  <RouterView name="DataView"></RouterView>
+  <RouterView name="ContentView"></RouterView>
+</template>
+
+<style scoped></style>
---
+++ src/modules/components/Layout/DefaultLayout.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ScreenTone></ScreenTone>
+  <MainFrame></MainFrame>
+</template>
+
+<style scoped></style>
