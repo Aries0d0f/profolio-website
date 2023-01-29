0dc32d512cd0f1220eeb810f3dbb658643b0c2ba
aries0d0f <aries0d0f@gmail.com>
2023-01-29 17:48:14 +0800

    feat(styles): Init DefaultLayout with simple stylesheets.

--- src/modules/components/Layout/DefaultLayout.vue
+++ src/modules/components/Layout/DefaultLayout.vue
@@ -5,5 +5,16 @@
   <MainFrame></MainFrame>
   <RouterView name="GFXView"></RouterView>
   <RouterView name="DataView"></RouterView>
-  <RouterView name="ContentView"></RouterView>
+  <div class="viewport">
+    <RouterView name="ContentView"></RouterView>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.viewport {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
