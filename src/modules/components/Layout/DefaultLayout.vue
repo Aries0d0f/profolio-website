43aa597bf7da54a9e57d0b10ef3b1ea59bad7ef5
aries0d0f <aries0d0f@gmail.com>
2023-01-29 17:44:52 +0800

    feat(view): Move router viewport to DefaultLayout component.

--- src/modules/components/Layout/DefaultLayout.vue
+++ src/modules/components/Layout/DefaultLayout.vue
@@ -3,4 +3,7 @@
 <template>
   <ScreenTone></ScreenTone>
   <MainFrame></MainFrame>
+  <RouterView name="GFXView"></RouterView>
+  <RouterView name="DataView"></RouterView>
+  <RouterView name="ContentView"></RouterView>
 </template>
